fd6861f4050b8744e918efc69289cadc3c395622
jianglin <1033553441@qq.com>
2021-04-06 18:07:40 +0800

    新建桥接文件

---
+++ SwiftBridging/SwiftBridging/header/testoc.h
@@ -0,0 +1,16 @@
+//
+//  testoc.h
+//  SwiftBridging
+//
+//  Created by dfsx6 on 2021/4/6.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface testoc : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SwiftBridging/SwiftBridging/header/testoc.m
@@ -0,0 +1,12 @@
+//
+//  testoc.m
+//  SwiftBridging
+//
+//  Created by dfsx6 on 2021/4/6.
+//
+
+#import "testoc.h"
+
+@implementation testoc
+
+@end
